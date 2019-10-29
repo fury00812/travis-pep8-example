d29b47fa89d867ff98c858da01b1db24c4c6c951
fury00812 <fury.00812@gmail.com>
2019-10-29 18:38:11 +0900

    initial commit

---
+++ ngcode.py
@@ -0,0 +1,11 @@
+import sys
+
+class Test():
+    def __init__(self):
+        pass
+
+
+    def train(self):
+        print('Test.train') 
+
+
---
+++ okcode.py
@@ -0,0 +1,9 @@
+import sys
+
+
+class Test():
+    def __init__(self):
+        pass
+
+    def train(self):
+        print('Test.train')
